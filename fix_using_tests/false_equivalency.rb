f3d11ccb86729fc71ddf445a78337a639472d343
Ronald Abreu <ra@bluebirdgroup.ltd>
2020-04-01 02:16:00 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -1,5 +1,5 @@
 # don't forget to add: require 'pry'
-require pry 
+require pry
 def get_user_input
   input = gets.chomp
   input.to_i
@@ -23,3 +23,4 @@ def runner
   prompt_user
   selection(get_user_input)
 end
+binding.pry
